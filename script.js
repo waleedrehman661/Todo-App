0ce10824be1f303f0419e5f55aafa76f808ce46e
Waleed-Rehman <waleedrehman2007@gmail.com>
2025-03-04 04:12:09 +0500

    Todo done

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var inputbox = document.querySelector("#input");
 var addbtn =document.querySelector("#add");
 var list = document.querySelector("#list");
+var removebtn = document.querySelector(".removetodo");
+var clearbtn = document.querySelector("#clearbtn");
 
 function Todo(){
     var inputvalue =inputbox.value;
@@ -9,8 +11,18 @@ function Todo(){
     }
      else{
         var li= document.createElement("li");
-        li.innerHTML = `${inputvalue} <i class="fa-regular fa-xmark"></i>`;
+        li.textContent = inputvalue;
+        var removeIcon = document.createElement("span"); 
+        removeIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 32 32"><path fill="none" stroke="#e01717" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 23L23 9m0 14L9 9"/></svg>`;
+        removeIcon.style.cursor = "pointer";   
         list.appendChild(li);
-        inputvalue =""
-     }
+        li.appendChild(removeIcon);
+        inputbox.value = "";
+    }
+    removeIcon.addEventListener("click", function() { 
+        li.remove(); 
+    });
+    clearbtn.addEventListener("click" , function(){
+        list.innerHTML="";
+    })
 }
